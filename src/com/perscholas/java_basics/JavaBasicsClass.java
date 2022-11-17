00f6a441a9bfa1f137a9a4ef2e5e2ea9be88fca2
Edward Nguyen <edwardlocnguyen@gmail.com>
2022-11-16 18:24:43 -0600

    completed most problems

---
+++ src/com/perscholas/java_basics/JavaBasicsClass.java
@@ -0,0 +1,19 @@
+package com.perscholas.java_basics;
+
+public class JavaBasicsClass {
+	
+	public static void main(String[] args) {
+
+		BulletPoints bulletPoint = new BulletPoints();
+		
+		bulletPoint.sumOfInts();
+		bulletPoint.sumOfDbls();
+		bulletPoint.sumOfIntAndDbl();
+		bulletPoint.divideTwoInts();
+		bulletPoint.divideTwoDbls();
+		bulletPoint.divideTwoIntsCast();
+		bulletPoint.useConst();
+		bulletPoint.cafe();
+
+	}
+}
